f7958a4ce3394fd1c802c178825917fdd63afd0f
YeoniSon <sjy8211@gmail.com>
2025-08-11 23:56:55 +0900

    feat: [03-HEADER] 헤더 구현

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import "./App.css";
+import Header from "./components/base/Header";
 import Homepage from "./components/pages/homepage/HomePage";
 import LoginPage from "./components/pages/user/LoginPage";
 import SignupPage from "./components/pages/user/SignupPage";
 
-function App() {
+// BrowserRouter 내부에서 useLocation을 사용하는 컴포넌트
+function AppContent() {
+  const location = useLocation();
+
+  // 현재 경로가 login, signup이면 false
+  const showHeader = !["/login", "/signup"].includes(location.pathname);
+
   return (
-    <div className="App">
-      <BrowserRouter>
+    <>
+      {showHeader && <Header />}
+      <div style={{ marginTop: showHeader ? "4rem" : "0" }}>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
         </Routes>
+      </div>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <AppContent />
       </BrowserRouter>
     </div>
   );
