6e1c5e5703f95f0651199e2b33a9a6de88d10d36
YeoniSon <sjy8211@gmail.com>
2025-08-13 00:10:41 +0900

    feat: Hompage-WelcomeSection 구현

--- src/components/pages/homepage/HomePage.js
+++ src/components/pages/homepage/HomePage.js
@@ -1,7 +1,41 @@
 import React from "react";
 
+import WelcomeSection from "../../features/homepage/WelcomeSection";
+import ActionSection from "../../features/homepage/ActionSection";
+
 const Homepage = () => {
-  return;
+  const actionItem = [
+    {
+      title: "티켓 거래",
+      description: "안전한 티켓 양도거래",
+      icon: "ticket",
+      path: "/ticket",
+    },
+    {
+      title: "선수 등/말소",
+      description: "선수들의 등록/말소 정보",
+      icon: "player",
+      path: "/player-input",
+    },
+    {
+      title: "실시간 채팅",
+      description: "팬들과의 실시간 소통",
+      icon: "chat",
+      path: "/community",
+    },
+    {
+      title: "순위표",
+      description: "팀 순위 및 선수 순위",
+      icon: "standings",
+      path: "/standings",
+    },
+  ];
+  return (
+    <div>
+      <WelcomeSection />
+      <ActionSection actionItems={actionItem} />
+    </div>
+  );
 };
 
 export default Homepage;
