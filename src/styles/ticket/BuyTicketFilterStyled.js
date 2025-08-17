890baae2d3790cc5633d80e1c91ba4365325c6a6
YeoniSon <sjy8211@gmail.com>
2025-08-17 23:51:41 +0900

    feat: 티켓 구매 구현

---
+++ src/styles/ticket/BuyTicketFilterStyled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const BuyTicketContainer = styled.div`
+  display: flex;
+  margin-top: 20px;
+  padding: 0 30px;
+  border: 1px solid rgba(135, 133, 133, 0.27);
+  border-radius: 8px;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const BuyTicketHeader = styled.h2`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
