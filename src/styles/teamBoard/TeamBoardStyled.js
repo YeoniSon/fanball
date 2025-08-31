f4cdab82fa8abaee96dcf050fd45c0ca7d1352a7
YeoniSon <sjy8211@gmail.com>
2025-08-31 13:51:03 +0900

    feat: 팀별 게시판 페이지 구현

---
+++ src/styles/teamBoard/TeamBoardStyled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const TitleRow = styled.span`
+  display: inline-flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const LogoImage = styled.img`
+  width: 70px;
+  height: 70px;
+  object-fit: contain;
+  margin-right: 10px;
+`;
