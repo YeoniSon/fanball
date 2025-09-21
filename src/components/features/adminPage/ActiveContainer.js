fc60d4c350f08ddd8f7ee4f86d8fee3e7d6ce27f
YeoniSon <sjy8211@gmail.com>
2025-09-21 21:15:41 +0900

    feat: 활동바 구현

--- src/components/features/adminPage/ActiveContainer.js
+++ src/components/features/adminPage/ActiveContainer.js
@@ -1,8 +1,62 @@
-const ActiveContainer = () => {
+import { useState } from "react";
+import {
+  Select,
+  SelectContainer,
+} from "../../../styles/ranking/SelectSectionStyled";
+import UserContent from "./posts/UserContent";
+
+const ActiveContainer = ({ user, reports, notices, admin }) => {
+  const [activeTab, setActiveTab] = useState("users");
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  const renderContent = () => {
+    switch (activeTab) {
+      case "users":
+        return <UserContent user={user} />;
+      case "reports":
+        return;
+      case "notices":
+        return;
+      case "admins":
+        return;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div>
-      <h1>Active Container</h1>
-    </div>
+    <>
+      <SelectContainer style={{ margin: "30px 0 10px 0" }}>
+        <Select
+          isActive={activeTab === "users"}
+          onClick={() => handleTabClick("users")}
+        >
+          사용자 관리
+        </Select>
+        <Select
+          isActive={activeTab === "admins"}
+          onClick={() => handleTabClick("admins")}
+        >
+          관리자 관리
+        </Select>
+        <Select
+          isActive={activeTab === "reports"}
+          onClick={() => handleTabClick("reports")}
+        >
+          신고 관리
+        </Select>
+        <Select
+          isActive={activeTab === "notices"}
+          onClick={() => handleTabClick("notices")}
+        >
+          공지사항 관리
+        </Select>
+      </SelectContainer>
+      {renderContent()}
+    </>
   );
 };
 export default ActiveContainer;
